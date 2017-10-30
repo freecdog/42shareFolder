cb47d078cc149c5dac58c0f56c37224490883377
freecdog <cdog@ya.ru>
2017-10-30 23:28:27 +0300

    added 'mka', 'ac3' to file formats; added --path argument to set up shared folder

--- app.js
+++ app.js
@@ -25,7 +25,69 @@ app.use(express.static(path.join(__dirname, 'public')));
 //app.sharedFolder = path.join('W:', '1');
 app.sharedFolder = path.join('X:', '1');
 //app.sharedFolder = path.join('X:', 'pro', '!codeschool.com');
-app.use(express.static( app.sharedFolder ));
+
+//app.use(express.static( app.sharedFolder ));
+checkPathArgv(function(err, argPath){
+    if (!err){
+        console.log('applied path from Argv:', argPath.path);
+        app.sharedFolder = argPath.path;
+    }
+    app.use(express.static( app.sharedFolder ));
+});
+
+function checkPathArgv(callback){
+    callback = callback || function(){};
+    //console.log(process.argv);
+
+    var argPath = {pathInArgv: false};
+    //app.argPath = argPath;
+
+    var pathIndexInArgv = process.argv.indexOf("--path");
+    if (pathIndexInArgv != -1) {
+        var fs = require('fs');
+
+        var pathToCheck = process.argv[pathIndexInArgv + 1];
+
+        if (pathToCheck !== undefined){
+            //var rec = path.parse(pathToCheck);
+            //pathToCheck = path.join(rec.dir, rec.base);
+            pathToCheck = path.normalize(pathToCheck);
+
+            // TODO using ExistsSync because it seems that express.static is not applied in parallel mode
+            // fileExist is deprecated, fileExistSync is not, fs.access is recommended
+            if (fs.existsSync(pathToCheck)) {
+                argPath = {
+                    pathInArgv: true,
+                    path: pathToCheck
+                };
+                callback(null, argPath);
+            } else {
+                callback({message: pathToCheck + ' not exist'}, null);
+            }
+            //fs.open(pathToCheck, 'r', function(err, fd){
+            //    if (err) {
+            //        if (err.code === 'ENOENT') {
+            //            console.error(pathToCheck, 'does not exist');
+            //            //return;
+            //        }
+            //        //throw err;
+            //        callback(err, null);
+            //    } else {
+            //        argPath = {
+            //            pathInArgv: true,
+            //            path: pathToCheck
+            //        };
+            //        //console.log(argPath);
+            //        callback(null, argPath);
+            //    }
+            //});
+        } else {
+            callback({message: 'no path given'});
+        }
+    } else {
+        callback({message: 'no --path argument'});
+    }
+}
 
 app.use('/', routes);
 app.use('/api', api);
